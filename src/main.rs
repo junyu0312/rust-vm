1fadc060cc15712d373c9ae74de730c0da80e9e4
Zhang Junyu <junyu92@gmail.com>
2025-12-26 13:28:32 +0000

    feat: Create vm
    Signed-off-by: Zhang Junyu <junyu92@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+use clap::Parser;
+use rust_vm::command::Command;
+use rust_vm::kvm::device::open_kvm;
+use rust_vm::kvm::vm::create_vm;
+use tracing::debug;
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let args = Command::parse();
+    debug!(?args);
+
+    let kvm_fd = open_kvm()?;
+    debug!(kvm_fd);
+
+    let vm_fd = create_vm(kvm_fd)?;
+    debug!(vm_fd);
+
+    Ok(())
 }
