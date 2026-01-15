38906bc731473b3dcd56ba0b00754cb1a0d96575
Zhang Junyu <junyu92@gmail.com>
2026-01-15 16:59:04 +0000

    fix: Support aux dor i8042

--- crates/vm-device/src/device/i8042/controller_cfg.rs
+++ crates/vm-device/src/device/i8042/controller_cfg.rs
@@ -2,19 +2,17 @@ use bitflags::bitflags;
 
 bitflags! {
     pub struct ControllerConfigurationByte: u8 {
-        const FirstPs2PortInterrupt = 1 << 0;
-        const SecondPs2PortInterrupt = 1 << 1;
-        const SystemFlag = 1 << 2;
-        const Zero0 = 1 << 3;
-        const FirstPs2PortClock = 1 << 4;
-        const SecondPs2PortClock = 1 << 5;
-        const FirstPs2PortTranslation = 1 << 6;
-        const Zero1 = 1 << 7;
+        const CTL_KBDINT = 1 << 0;
+        const CTL_AUXINT = 1 << 1;
+        const CTL_IGNKEYLOCK = 1 << 3;
+        const CTL_KBDDIS = 1 << 4;
+        const CTL_AUXDIS = 1 << 5;
+        const CTL_XLATE = 1 << 6;
     }
 }
 
 impl Default for ControllerConfigurationByte {
     fn default() -> Self {
-        ControllerConfigurationByte::SystemFlag | ControllerConfigurationByte::FirstPs2PortInterrupt
+        ControllerConfigurationByte::empty()
     }
 }
--- crates/vm-device/src/device/i8042/status_register.rs
+++ crates/vm-device/src/device/i8042/status_register.rs
@@ -2,21 +2,19 @@ use bitflags::bitflags;
 
 bitflags! {
     pub struct StatusRegister: u8 {
-        const OBF = 1 << 0;
-        const IBF = 1 << 1;
-        const SystemFlag = 1 << 2;
-        const Command = 1 << 3;
-        const Keylock = 1 << 4;
-        const AuxData = 1 << 5;
-        const TimeoutError = 1 << 6;
-        const ParityError = 1 << 7;
+        const STR_OBF = 1 << 0;
+        const STR_IBF = 1 << 1;
+        const STR_MUXERR = 1 << 2;
+        const STR_CMDDAT = 1 << 3;
+        const STR_KEYLOCK = 1 << 4;
+        const STR_AUXDATA = 1 << 5;
+        const STR_TIMEOUT = 1 << 6;
+        const STR_PARITY = 1 << 7;
     }
 }
 
 impl Default for StatusRegister {
     fn default() -> Self {
-        let mut reg = StatusRegister::empty();
-        reg.insert(StatusRegister::SystemFlag);
-        reg
+        StatusRegister::STR_KEYLOCK // Avoid linux warning
     }
 }
