1c210fd22480c18fea2f501c58c4989ed0c674f2
Zhang Junyu <junyu92@gmail.com>
2026-02-13 21:06:27 +0800

    refine: Refine device-tree generating for pci

--- crates/vm-pci/src/root_complex/mmio.rs
+++ crates/vm-pci/src/root_complex/mmio.rs
@@ -13,47 +13,11 @@ use crate::root_complex::PciRootComplex;
 use crate::root_complex::mmio::bar_handler::DeviceMmioHandler;
 use crate::root_complex::mmio::ecam_handler::EcamHandler;
 
+mod arch;
 mod bar_handler;
 mod ecam_handler;
 
 type PciToGpaMapping = AddressSpace<u64, u64>;
-
-#[cfg(target_arch = "x86_64")]
-fn todo_irq() {
-    todo!()
-}
-
-#[cfg(target_arch = "aarch64")]
-struct InterruptMapEntry {
-    pci_addr_high: u32,
-    pci_addr_mid: u32,
-    pci_addr_low: u32,
-    pin: u32,
-    gic_phandle: u32,
-    gic_addr_high: u32,
-    gic_addr_low: u32,
-    gic_irq_type: u32,
-    gic_irq_num: u32,
-    gic_irq_flags: u32,
-}
-
-#[cfg(target_arch = "aarch64")]
-impl InterruptMapEntry {
-    fn into_array(self) -> [u32; 10] {
-        [
-            self.pci_addr_high,
-            self.pci_addr_mid,
-            self.pci_addr_low,
-            self.pin,
-            self.gic_phandle,
-            self.gic_addr_high,
-            self.gic_addr_low,
-            self.gic_irq_type,
-            self.gic_irq_num,
-            self.gic_irq_flags,
-        ]
-    }
-}
 
 #[derive(Debug)]
 struct DeviceSel {
@@ -167,36 +131,7 @@ impl MmioDevice for PciRootComplexMmio {
         fdt.property_array_u32("bus-range", &[0, 0])?;
         fdt.property_array_u64("reg", &[self.ecam_range.start, self.ecam_range.len as u64])?;
 
-        #[cfg(target_arch = "aarch64")]
-        {
-            use vm_core::irq::Phandle;
-            use vm_core::irq::arch::aarch64::GIC_SPI;
-            use vm_core::irq::arch::aarch64::IRQ_TYPE_LEVEL_HIGH;
-
-            use crate::device::interrupt::legacy::InterruptPin;
-
-            fdt.property_array_u32("interrupt-map-mask", &[0, 0, 0, 7])?;
-            // TODO: hard code, virtio-pci-blk
-            let entry = InterruptMapEntry {
-                pci_addr_high: 0x800,
-                pci_addr_mid: 0,
-                pci_addr_low: 0,
-                pin: InterruptPin::INTA as u32,
-                gic_phandle: Phandle::GIC as u32,
-                gic_addr_high: 0,
-                gic_addr_low: 0,
-                gic_irq_type: GIC_SPI,
-                gic_irq_num: 10,
-                gic_irq_flags: IRQ_TYPE_LEVEL_HIGH,
-            };
-            fdt.property_array_u32("interrupt-map", &entry.into_array())?;
-            fdt.property_u32("msi-parent", Phandle::MSI as u32)?;
-        }
-
-        #[cfg(target_arch = "x86_64")]
-        {
-            todo_irq();
-        }
+        self.generate_device_tree_arch(fdt)?;
 
         fdt.end_node(node)?;
 
---
+++ crates/vm-pci/src/root_complex/mmio/arch/x86_64.rs
@@ -0,0 +1,7 @@
+use crate::root_complex::mmio::PciRootComplexMmio;
+
+impl PciRootComplexMmio {
+    pub fn generate_device_tree_arch(&self) -> Result<(), vm_fdt::Error> {
+        todo!()
+    }
+}
