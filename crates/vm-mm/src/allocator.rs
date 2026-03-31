51d5674e92ae580c42539cfab6c5e9daa07d298c
Zhang Junyu <junyu92@gmail.com>
2026-03-31 21:27:00 +0800

    refine: Replace applevisor with applevisor_sys for flexibility

--- crates/vm-mm/src/allocator.rs
+++ crates/vm-mm/src/allocator.rs
@@ -2,6 +2,7 @@ use crate::error::Error;
 use crate::memory_container::MemoryContainer;
 
 pub mod mmap_allocator;
+pub mod std_allocator;
 
 pub trait Allocator {
     type Container: MemoryContainer;
