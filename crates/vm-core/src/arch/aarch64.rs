88e70be3c3fbd04ff954a91edd16c40b44e53c54
Zhang Junyu <junyu92@gmail.com>
2026-04-02 13:55:15 +0800

    refine: Clean code

--- crates/vm-core/src/arch/aarch64.rs
+++ crates/vm-core/src/arch/aarch64.rs
@@ -1,26 +1,5 @@
-use crate::arch::Arch;
-use crate::arch::aarch64::layout::AArch64Layout;
-use crate::arch::aarch64::vm_exit::VmExitReason;
-
 pub mod firmware;
 pub mod irq;
 pub mod layout;
 pub mod vcpu;
 pub mod vm_exit;
-
-pub struct AArch64 {
-    pub layout: AArch64Layout,
-}
-
-impl Arch for AArch64 {
-    type VmExitReason = VmExitReason;
-    type Layout = AArch64Layout;
-
-    fn get_layout(&self) -> &AArch64Layout {
-        &self.layout
-    }
-
-    fn get_layout_mut(&mut self) -> &mut AArch64Layout {
-        &mut self.layout
-    }
-}
--- crates/vm-core/src/arch/x86_64.rs
+++ crates/vm-core/src/arch/x86_64.rs
@@ -1,24 +1,5 @@
-use crate::arch::Arch;
-use crate::arch::x86_64::layout::X86_64Layout;
-use crate::arch::x86_64::vm_exit::VmExitReason;
-
 pub mod layout;
 pub mod vcpu;
 pub mod vm_exit;
 
 pub const BASE_ADDRESS: u64 = 0x0;
-
-pub struct X86_64;
-
-impl Arch for X86_64 {
-    type VmExitReason = VmExitReason;
-    type Layout = X86_64Layout;
-
-    fn get_layout(&self) -> &X86_64Layout {
-        todo!()
-    }
-
-    fn get_layout_mut(&mut self) -> &mut X86_64Layout {
-        todo!()
-    }
-}
