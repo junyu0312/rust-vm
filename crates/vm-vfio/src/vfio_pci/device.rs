6ec53e02e3df54f36cba41b1afc9a207a181c171
Zhang Junyu <junyu92@gmail.com>
2026-06-11 06:19:23 +0000

    feat: Remove address from header

--- crates/vm-vfio/src/vfio_pci/device.rs
+++ crates/vm-vfio/src/vfio_pci/device.rs
@@ -53,6 +53,17 @@ impl VfioPciDevice {
                 return Err(Error::VfioPciDeviceIsNotEndpoint);
             }
 
+            // clear address
+            for index in 0..6 {
+                if header.bar[index] & PCI_BASE_ADDRESS_SPACE == 0 {
+                    // Memory
+                    header.bar[index] &= 0xf;
+                } else {
+                    // Io
+                    header.bar[index] &= 0x3;
+                }
+            }
+
             // TODO: Should we emulate irq_line?
             // TODO: Should we reconstruct cap?
             // TODO: Should we emulate status?
